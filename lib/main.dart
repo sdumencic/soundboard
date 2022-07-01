60db04484697809cd59707ec661a386e21478893
sdumencic <stella.dumencic@gmail.com>
2022-07-02 00:05:52 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sound Board',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Sound Board'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final player = AudioPlayer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        width: double.infinity,
+        color: Colors.grey[900],
+        child: Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.9,
+            height: MediaQuery.of(context).size.width * 0.9,
+
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.blue[900],
+            ),
+            child: GridView.count(
+              primary: false,
+              crossAxisCount: 5,
+              mainAxisSpacing: 5,
+              crossAxisSpacing: 5,
+              padding: const EdgeInsets.all(10),
+
+              children: List.generate(25, (index) {
+                return InkWell(
+                  onTap: () => {
+                    player.play(AssetSource('audio/1.mp3'))
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      // boxShadow: [
+                      //   BoxShadow(color: Colors.white, spreadRadius: 3),
+                      // ],
+                    ),
+                    // child: Text(
+                    //   '$index',
+                    //   style: Theme.of(context).textTheme.headline5,
+                    // ),
+                  ),
+                );
+              }),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
